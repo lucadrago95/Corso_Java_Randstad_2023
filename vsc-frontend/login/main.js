a928f196d830d8460ff05943826630e9cc89f28a
lucadrago95 <127793221+lucadrago95@users.noreply.github.com>
2023-11-06 23:02:31 +0100

    finito script per gestire operazioni bacarie

--- vsc-frontend/login/main.js
+++ vsc-frontend/login/main.js
@@ -9,11 +9,22 @@ const btn_home = document.getElementById("btn_home");
 const btn_deposito = document.getElementById("btn_deposito");
 const btn_prelievo = document.getElementById("btn_prelievo");
 const btn_out = document.getElementById("btn_out");
-// 
+// output della card + cambia contenuto dinamico
 const card = document.getElementById("out_screen");
+// home e saldo
 const card_saldo = document.getElementById("saldo");
+const out_saldo = document.getElementById("out_saldo");
+// deposito e prelievo
+// contenuto deposito
 const card_deposito = document.getElementById("deposito");
+// input deposito
+const in_deposito = document.getElementById("in_deposito");
+// contenuto prelievo
 const card_prelievo = document.getElementById("prelievo");
+// input prelievo
+const in_prelievo = document.getElementById("in_prelievo");
+
+
 // funzione che controlla quando premi enter
 login.addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
@@ -32,6 +43,7 @@ function controllaPin(){
         login_container.classList.add("hidden");
         console.log("ho tolto il login");
         home_container.classList.remove("hidden");
+        mostraSaldo();
 
     } else {
         console.log("pin errato");
@@ -39,14 +51,20 @@ function controllaPin(){
     }
 }
 
-btn_home.addEventListener("click", pressBtnHome)
+btn_home.addEventListener("click", homeSaldo);
+// piccola funzione per richiamare 2 funzioni al click del bottone home
+function homeSaldo() {
+    pressBtnHome();
+    mostraSaldo();
+}
 function pressBtnHome(){
     // const card = document.getElementById("out_screen");
     console.log("click home");
     card_saldo.classList.remove("hidden")
     card_deposito.classList.add("hidden");
-    card_prelievo.classList.add("hidden");
+    card_prelievo.classList.add("hidden");    
 }
+
 btn_deposito.addEventListener("click", pressBtnDeposito)
 function pressBtnDeposito(){
     // const card = document.getElementById("out_screen");
@@ -54,6 +72,7 @@ function pressBtnDeposito(){
     card_deposito.classList.remove("hidden");
     card_saldo.classList.add("hidden")
     card_prelievo.classList.add("hidden");
+
 }
 btn_prelievo.addEventListener("click", pressBtnPrelievo)
 function pressBtnPrelievo(){
@@ -71,5 +90,51 @@ function pressBtnOut(){
     login_container.classList.remove("hidden");
 }
 
+// costanti per lavorare all'algorito di calcolo per le operazioni 
+var saldo = 2000;
+var deposito = 0;
+var prelievo = 0;
+
+function mostraSaldo() {
+    out_saldo.textContent = saldo;
+    console.log(saldo);
+    // alert(saldo)
+}
+
+// funzione che controlla quando premi enter dentro la card deposito
+in_deposito.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        //fa partire la funzione
+        console.log("premo enter in deposito");
+        incrementaSaldo();
+    }
+});
+function incrementaSaldo() {
+    let valoreDepositato = parseFloat(in_deposito.value);
+    saldo += valoreDepositato;
+    in_deposito.value = "";
+    mostraSaldo();
+    console.log("ho depositato i soldi " + valoreDepositato);
+}
 
 
+// funzione che controlla quando premi enter dentro la card prelievo
+in_prelievo.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        //fa partire la funzione
+        console.log("premo enter in deposito");
+        decrementaSaldo();
+    }
+});
+
+function decrementaSaldo() {
+    let valorePrelevato = parseFloat(in_prelievo.value);
+    saldo -= valorePrelevato;
+    in_prelievo.value = "";
+    mostraSaldo();
+    console.log("ho prelevato i soldi " + valorePrelevato);
+}
+
+
+// da rimuovere alla fine 
+// window.addEventListener("load", mostraSaldo);
